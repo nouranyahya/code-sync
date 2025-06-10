5b2acac0c3c560804968d2e499dd92f0ddc1ac59
Nouran Yahya <nourannyahya@gmail.com>
2025-06-09 21:20:20 -0400

    Add solution for is-subsequence

---
+++ leetcode/is-subsequence.py
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        if(s.length()==0 && t.length()==0){return true;}
+        int k=0;
+        for(int i=0; i<t.length(); i++){
+            if(s[k]==t[i]){
+                k++;
+            }
+            if (k==s.length()){
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
