ca944f13f8ee3dd860637faf89074a600c5b304d
Nouran Yahya <nourannyahya@gmail.com>
2025-06-09 21:20:06 -0400

    Add solution for h-index

---
+++ leetcode/h-index.py
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+
+        std::sort(citations.begin(), citations.end());
+
+        for(int i=0; i<citations.size(); i++){
+            if(citations[i]>=citations.size()-i){
+                return citations.size()-i;
+            }
+        }
+
+        return citations[citations.size()/2];
+    }
+};
